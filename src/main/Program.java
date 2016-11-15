086628f1c3d69821cb2a45589fdc29a326802cea
Hannah Smyth-Osbourne <hosbourn@thoughtworks.com>
2016-11-15 10:02:50 +0000

    Extract printOptions method from main method in Program

--- src/main/Program.java
+++ src/main/Program.java
@@ -9,19 +9,7 @@ public class Program {
 
     public static void main(String[] args) {
         while (true) {
-            System.out.println("**********************************************************");
-            System.out.println("* Welcome to The Bangalore Public Library System - Biblioteca *");
-            System.out.println("**********************************************************");
-            System.out.println("*                Menu                                    *");
-            System.out.println("*         =====================                          *");
-            System.out.println("*         1. List Book Catalog                           *");
-            System.out.println("*         2. Check out Book                              *");
-            System.out.println("*         3. Check Library Number                        *");
-            System.out.println("*         4. Movie Listing                               *");
-            System.out.println("*         5. Login                                       *");
-            System.out.println("*         9. Exit                                        *");
-            System.out.println("**********************************************************");
-            System.out.println("Your Selection: ");
+            printOptions();
 
             InputStreamReader inputStream = new InputStreamReader(System.in);
             BufferedReader reader = new BufferedReader(inputStream);
@@ -119,6 +107,22 @@ public class Program {
                 System.out.println("Enter a valid integer!!");
             }
         }
+    }
+
+    private static void printOptions() {
+        System.out.println("**********************************************************");
+        System.out.println("* Welcome to The Bangalore Public Library System - Biblioteca *");
+        System.out.println("**********************************************************");
+        System.out.println("*                Menu                                    *");
+        System.out.println("*         =====================                          *");
+        System.out.println("*         1. List Book Catalog                           *");
+        System.out.println("*         2. Check out Book                              *");
+        System.out.println("*         3. Check Library Number                        *");
+        System.out.println("*         4. Movie Listing                               *");
+        System.out.println("*         5. Login                                       *");
+        System.out.println("*         9. Exit                                        *");
+        System.out.println("**********************************************************");
+        System.out.println("Your Selection: ");
     }
 
     private static boolean validPassword(String password) {
