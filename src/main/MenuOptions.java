f21adcf2af5b0d61722aca6282ae5fa6d7d1e3ba
Hannah Smyth-Osbourne <hannahsmyth-osbourne@Hannahs-MacBook-Pro.local>
2016-11-18 16:22:49 +0000

    Extract MenuOptions into its own class

---
+++ src/main/MenuOptions.java
@@ -0,0 +1,24 @@
+package main;
+
+/**
+ * Created by hannahsmyth-osbourne on 18/11/2016.
+ */
+public class MenuOptions {
+
+   public void print() {
+        System.out.println("**********************************************************");
+        System.out.println("* Welcome to The Bangalore Public Library System - Biblioteca *");
+        System.out.println("**********************************************************");
+        System.out.println("*                Menu                                    *");
+        System.out.println("*         =====================                          *");
+        System.out.println("*         1. List Book Catalog                           *");
+        System.out.println("*         2. Check out Book                              *");
+        System.out.println("*         3. Check Library Number                        *");
+        System.out.println("*         4. Movie Listing                               *");
+        System.out.println("*         5. Login                                       *");
+        System.out.println("*         9. Exit                                        *");
+        System.out.println("**********************************************************");
+        System.out.println("Your Selection: ");
+    }
+
+}
